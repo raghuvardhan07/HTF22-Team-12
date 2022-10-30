eccc7cff8e7825c044c00abf06351bf2685ac68a
Raghu_Vardhan <your_email_address@example.com>
2022-10-30 13:37:41 +0530

    Project

---
+++ jsint2.js
@@ -0,0 +1,96 @@
+let loc =document.getElementById("location");
+let tempicon=document.getElementById("temp-icon");
+let tempvalue=document.getElementById("temp-value");
+// let climate =document.getElementById("climate");
+let iconfile;
+const searchInput=document.getElementById("search-input");
+const searchButton=document.getElementById("search-button");
+
+searchButton.addEventListener('click', (e)=>
+{
+
+e.preventDefault();
+getWeather(searchInput.value);
+searchInput.value='';
+
+
+});
+
+
+
+const getWeather=async (city)=>
+{
+    try{
+
+        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=278ba629def8e2d59fd6c8c7145b70ed`
+   
+            ,{mode: 'cors'}
+        );
+
+        const weatherData= await response.json();
+        console.log(weatherData);
+        const{name}=weatherData;
+        const{feels_like}=weatherData.main;
+        const{id,main}=weatherData.weather[0];
+        loc.textContent=name;
+        climate.textContent=main;
+        tempvalue.textContent=Math.round(feels_like-273);
+    }
+catch(error)
+{
+    alert('city not found');
+}
+};
+
+
+
+window.addEventListener("load" ,()=>{
+
+let long;
+let lat;
+
+if(navigator.geolocation)
+{
+
+    navigator.geolocation.getCurrentPosition((position)=>
+    {
+
+   
+    
+    long=position.coords.longitude;
+    lat=position.coords.latitude;
+    const proxy="https://cors-anywhere.herokuapp.com/";
+
+        const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=278ba629def8e2d59fd6c8c7145b70ed`
+
+        fetch(api).then((response)=>{
+
+            return response.json();
+
+
+        })
+
+        .then (data =>
+            {
+
+                    const{name}=data;
+                    const{feels_like}=data.main;
+                    const{id,main}=data.weather[0];
+                    loc.textContent=name;
+                    climate.textContent=main;
+                    tempvalue.textContent=Math.round(feels_like-273);
+                    console.log(data);
+
+
+            })
+
+
+
+}
+    
+    
+    
+    )}
+
+
+})
